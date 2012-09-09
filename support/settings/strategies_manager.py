74c6b94844d4a0866f3b19f6feae60b4d75244bf
Alexey Plutalov <demiazz.py@gmail.com>
2012-09-10 00:12:08 +0400

    Adds loading and compiling formats into regexp.

--- support/settings/strategies_manager.py
+++ support/settings/strategies_manager.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import re
+
 import sublime
 
 
@@ -13,4 +15,35 @@ class StrategiesManager:
   """
 
   def __init__(self):
+    default = sublime.load_settings(DEFAULT_STRATEGIES_FILE)
+    user    = sublime.load_settings(USER_STRATEFIES_FILE)
+
+    self.formats = self.__extract_formats(default, user)
     return
+
+  #----- Formats dealing
+
+  def __extract_formats(self, default, user):
+    """
+    Extract default and user formats and compile them.
+    """
+    raw_formats = default.get("formats", {})
+    raw_formats.update(user.get("formats", {}))
+
+    formats = {}
+
+    for name, pattern in raw_formats.iteritems():
+      formats[name] = self.__compile_format(pattern)
+
+    return formats
+
+  def __compile_format(self, format):
+    """
+    Compile format to regexp.
+
+    * escape regexp characters in string;
+    * replace `$encoding$` substring to group;
+    * compile regexp.
+    """
+    return re.compile(re.escape(format)
+                        .replace("\$encoding\$", "(?P<encoding> [\w|\d|\-]+)"))
