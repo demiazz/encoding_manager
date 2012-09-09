824e63c61b5ddb5f5e3241ebe35c43d6c783ebc8
Alexey Plutalov <demiazz.py@gmail.com>
2012-09-09 20:03:16 +0400

    add framework for detecting encoding declaration.

--- support/mixins/detector_mixin.py
+++ support/mixins/detector_mixin.py
@@ -3,7 +3,8 @@
 import re
 
 
-SHEBANG_REGEXP = re.compile(r'^#!.*$')
+SHEBANG_PATTERN   = r'^#!.*$'
+ENCODING_PATTERNS = []
 
 
 class DetectorMixin:
@@ -11,10 +12,29 @@ class DetectorMixin:
   Bundle of usefull methods for detecting headers of the file.
   """
 
+  def __init__(self, user_patterns = list()):
+    self.__shebang_pattern = re.compile(SHEBANG_PATTERN)
+    self.__encoding_patterns = [re.compile(p)
+                                  for p in ENCODING_PATTERNS + user_patterns]
+    return
+
   def is_shebang_exists(self, view):
     """
     Detect existing of shebang header in editing file.
     """
     line = view.substr(view.full_line(0))
+    return not not self.__shebang_pattern.match(line)
 
-    return not not SHEBANG_REGEXP.match(line)
+  def is_encoding_exists(self, view):
+    """
+    Detect existing of encoding declaration in editing file.
+    """
+    if self.is_shebang_exists(view):
+      line = view.substr(view.full_line(1))
+    else:
+      line = view.substr(view.full_line(0))
+
+    for pattern in self.__encoding_patterns:
+      if pattern.match(line):
+        return True
+    return False
