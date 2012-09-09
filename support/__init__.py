631a3fa517c188bd6dd852890539db8af86d3204
Alexey Plutalov <demiazz.py@gmail.com>
2012-09-10 00:39:25 +0400

    Move settings submodules to parent module.

--- support/__init__.py
+++ support/__init__.py
@@ -1,4 +1,1 @@
 # coding: utf-8
-
-from mixins import DetectorMixin, MatchMixin
-from settings import SettingsManager, StrategiesManager
--- support/settings/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-
-from settings_manager import SettingsManager
-from strategies_manager import StrategiesManager
